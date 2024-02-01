be304972b8f3b887a4680d89056dd78cf8a1d743
Antonio Barrios <barrios.tony@gmail.com>
2024-02-01 11:16:45 -0600

    Adding React Bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Results from './components/Results'
 import Transactions from './components/Transactions'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
