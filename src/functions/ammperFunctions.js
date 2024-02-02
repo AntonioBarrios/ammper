e7f9d19fdeb3a3e15a8ee30c2f2538eee3446ad0
Antonio Barrios <barrios.tony@gmail.com>
2024-02-02 13:56:08 -0600

    Change url to test

--- src/functions/ammperFunctions.js
+++ src/functions/ammperFunctions.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const allOperations = async () => {
     const request = await axios.get('https://jsonplaceholder.typicode.com/todos')
+    console.log(request)
 }
 
 export {
